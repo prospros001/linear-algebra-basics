d4b51c8c7738dcdfda6b54660fe9856958d84888
prospros001 <prospros@naver.com>
2020-11-17 15:20:45 +0900

    함수 #1

---
+++ 04/ex01.py
@@ -0,0 +1,19 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+def f(x):
+    return 2**x
+
+data_x = np.arange(-2, 5, 0.1)
+data_y = f(data_x)
+
+fig, splt = plt.subplots()
+plt.axvline(x=0, color='r')
+plt.axhline(y=0, color='r')
+
+splt.plot(data_x, data_y)
+splt.set_xticks([])
+splt.set_yticks([])
+
+plt.show()
