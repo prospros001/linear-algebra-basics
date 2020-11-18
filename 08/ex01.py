57e748f6835f781ff879008f2cd05c03b17f22a8
prospros001 <prospros@naver.com>
2020-11-18 14:09:45 +0900

    모듈 common 만들기

--- 08/ex01.py
+++ 08/ex01.py
@@ -1,32 +1,21 @@
 # 기울기(gradient)
-
+import sys
 import numpy as np
+try:
+    sys.path.append('D:\deep-learning\PycharmProjects\linear-algebra-basics\lib')
+    import common as cm
+except ImportError:
+    print('Library Module, Can Not Found')
+
 
 def f(x):
     return np.sum(x**2, axis=0)
-
-def numerical_gradient(f, x):
-    h = 1.0e-4
-    gradient = np.zeros_like(x)
-
-    for i in range(x.size):
-        tmp = x[i]
-        x[i] = tmp + h
-        h1 = f(x)
-
-        x[i] = tmp - h
-        h2 = f(x)
-
-        gradient[i] = (h1 - h2) / (2 * h)
-        x[i] = tmp
-
-    return gradient
 
 
 # 함수 테스트
 # print(f(np.array([3., 4., 5.])))
 
-gra1 = numerical_gradient(f, np.array([3., 4.]))
-gra2 = numerical_gradient(f, np.array([0., 2.]))
-gra3 = numerical_gradient(f, np.array([3., 0.]))
+gra1 = cm.numerical_gradient(f, np.array([3., 4.]))
+gra2 = cm.numerical_gradient(f, np.array([0., 2.]))
+gra3 = cm.numerical_gradient(f, np.array([3., 0.]))
 print(gra1, gra2, gra3)
---
+++ lib/common.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def numerical_gradient(f, x):
+    h = 1.0e-4
+    gradient = np.zeros_like(x)
+
+    for i in range(x.size):
+        tmp = x[i]
+        x[i] = tmp + h
+        h1 = f(x)
+        x[i] = tmp - h
+        h2 = f(x)
+        gradient[i] = (h1 - h2) / (2 * h)
+        x[i] = tmp
+
+    return gradient
