e541de432b339f1a9b8b11e0d1cb45427c83f7c5
prospros001 <prospros@naver.com>
2020-11-18 14:24:21 +0900

    모듈 common 만들기

--- 08/ex01.py
+++ 08/ex01.py
@@ -1,11 +1,14 @@
 # 기울기(gradient)
+import os
 import sys
+from pathlib import Path
 import numpy as np
 try:
+    # sys.path.append(os.path.join(Path(os.getcwd()).parent, 'lib'))
     sys.path.append('D:\deep-learning\PycharmProjects\linear-algebra-basics\lib')
-    import common as cm
+    from common import numerical_gradient
 except ImportError:
-    print('Library Module, Can Not Found')
+    print('Library Module Can Not Found')
 
 
 def f(x):
@@ -13,9 +16,10 @@ def f(x):
 
 
 # 함수 테스트
-# print(f(np.array([3., 4., 5.])))
+# print(f(np.array([3., 4.])))
 
-gra1 = cm.numerical_gradient(f, np.array([3., 4.]))
-gra2 = cm.numerical_gradient(f, np.array([0., 2.]))
-gra3 = cm.numerical_gradient(f, np.array([3., 0.]))
+gra1 = numerical_gradient(f, np.array([3., 4.]))
+gra2 = numerical_gradient(f, np.array([-1., -1.5]))
+gra3 = numerical_gradient(f, np.array([-0.25, -0.25]))
+
 print(gra1, gra2, gra3)
